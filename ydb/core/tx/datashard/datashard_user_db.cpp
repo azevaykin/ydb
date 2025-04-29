b26cd2a93f740d4ddd5dda7181b3b7fb458484d8
azevaykin <145343289+azevaykin@users.noreply.github.com>
2025-04-29 13:05:02 +0300

    Precharge in EvWrite (#17721)

--- ydb/core/tx/datashard/datashard_user_db.cpp
+++ ydb/core/tx/datashard/datashard_user_db.cpp
@@ -178,6 +178,16 @@ void TDataShardUserDb::EraseRow(
     Counters.EraseRowBytes += keyBytes + 8;
 }
 
+bool TDataShardUserDb::PrechargeRow(
+    const TTableId& tableId,
+    const TArrayRef<const TRawTypeValue> key)
+{
+    auto localTableId = Self.GetLocalTableId(tableId);
+    Y_ABORT_UNLESS(localTableId != 0, "Unexpected PrechargeRow for an unknown table");
+
+    return Db.Precharge(localTableId, key, key, {}, 0, Max<ui64>(), Max<ui64>());     
+}
+
 void TDataShardUserDb::IncreaseUpdateCounters(
     const TArrayRef<const TRawTypeValue> key, 
     const TArrayRef<const NIceDb::TUpdateOp> ops) 
